9905b602a659369f8633c3e0bf597a6f0a4b4c7f
Kanwar Anus <kunwaranus31@gmail.com>
2024-06-29 15:41:31 +0500

    Add first class folder with React project

---
+++ first class/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='align-item:center'>
+      <h1>Hello React</h1>
+      <p>Welcome to your first React app using Vite!</p>
+      <img width='300px' src='./src/assets/react.svg' />
+    </div>
+  )
+}
+
+export default App
